6f50e46400974eb46550446ec07630eec995a326
Suril Waghmare <waghmaresuril@gmail.com>
2025-08-01 19:41:25 -0400

    Removed errors

--- frontend/PopcornPlay/src/App.jsx
+++ frontend/PopcornPlay/src/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import Search from './components/Search';
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
-import {useDebounce} from 'react-use'
 import { getTrendingMovies, updateSearchCount } from './appwrite';
 const API_BASE_URL = "https://api.themoviedb.org/3/";
 
